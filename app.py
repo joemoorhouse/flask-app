493928c2ac595d1945853f305501e267d1f09fb2
Joe Moorhouse <joe.moorhouse@gmail.com>
2022-01-21 23:36:21 +0000

    Remove environment variable

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!" + os.environ['BUCKET_NAME']
+    return "Hello World!" # + os.environ['BUCKET_NAME']
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
