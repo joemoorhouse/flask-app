1e087e84b3f96825bfd058fda646e7905e47bd19
Joe Moorhouse <joe.moorhouse@gmail.com>
2022-01-21 22:53:40 +0000

    Environment variable injection

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from waitress import serve
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World!" + os.environ['BUCKET_NAME']
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
